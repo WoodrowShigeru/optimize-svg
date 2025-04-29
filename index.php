c76f1bd479572e12ba30d436b1b27264fca891b1
Woodrow Shigeru <woodrow.shigeru@gmx.net>
2025-04-29 22:30:22 +0200

    refactor: as class

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
  * @author WoodrowShigeru <woodrow.shigeru@gmx.net>
  */
 
+// use OptimizeSvg\Optimizer;
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -18,103 +20,16 @@ require_once implode(DIRECTORY_SEPARATOR, [
 	'autoload.php',
 ]);
 
+require_once implode(DIRECTORY_SEPARATOR, ['.', 'src', 'Optimizer.php']);
+
 
 // $input_file = implode(DIRECTORY_SEPARATOR, ['.', 'examples', 'tiles.svg']);
 $input_file = implode(DIRECTORY_SEPARATOR, ['.', 'examples', 'dice-6.svg']);
 $output_file = implode(DIRECTORY_SEPARATOR, ['.', 'examples', 'output.svg']);
 
-ivd([
-	'file-exists'	=> file_exists($input_file),
-	'readable'		=> is_readable($input_file),
-]);
 
-if (FALSE) {
-	throw new Exception('InputFileNotReadable');
-}
-
-
-// -----
-
-/**
- * @param DOMElement $node
- * @return void
- */
-function remove_hidden_children( $node ) {
-
-	$homunculus = new DOMNode();
-
-	if (!($node instanceof DOMElement)) {
-		return;
-		return $homunculus;
-	}
-
-
-	$deletables = array_filter(
-		iterator_to_array($node->childNodes),
-		fn($node) => $node instanceof DOMElement
-		&&	$node->hasAttribute('display')
-		&&	$node->getAttribute('display') === 'none'
-	);
-
-	foreach ($deletables as $child) {
-		$child->parentNode->removeChild($child);
-	}
-
-	return;
-	return $homunculus;
-}
-
-
-/**
- * @param DOMElement $node
- * @return void
- */
-function funky_recursion( $node ) {
-
-	// TODO  node or element?
-
-	if (!($node instanceof DOMElement)) {
-		return;
-		return new DOMElement();
-	}
-
-	if (!$node->childNodes->length) {
-		return;
-	}
-
-	remove_hidden_children($node);
-
-	foreach ($node->childNodes as $child) {
-		funky_recursion($child);
-	};
-}
-
-
-// -----
-
-$dom = new DOMDocument();
-
-$dom->load($input_file);
-// TODO  trycatch.
-
-$root = $dom->getElementsByTagName('svg');
-
-if ($root->length !== 1) {
-	throw new Exception('NoCleanSvg');
-}
-
-
-
-ivd($root[0]->childNodes->length, "before");
-
-funky_recursion($root[0]);
-// $samba = remove_hidden_children($root[0]);
-
-$samba = $root[0];
-ivd($samba->childNodes->length, "after");
-
-ivd($dom->saveHTML());
-
-file_put_contents($output_file, $dom->saveXML());
-// include $input_file;
+$optimizer = new Optimizer($input_file, $output_file, TRUE);
+$optimizer->optimize();
+ivd($optimizer->dump());
+$optimizer->save();
 
---
+++ src/Optimizer.php
@@ -0,0 +1,194 @@
+<?php
+/**
+ * dingdong class file.
+ *
+ * @since 0.0.1
+ * @author WoodrowShigeru <woodrow.shigeru@gmx.net>
+ */
+
+// namespace OptimizeSvg;
+
+// use DOMDocument;
+// use DOMElement;
+// use Exception;
+
+
+/**
+ *
+ */
+class Optimizer {
+
+	private bool $is_dev;
+	private string $input_file;
+	private string $output_file;
+
+	private DOMDocument $dom;
+	private DOMElement $root;
+
+
+	/**
+	 * Create an instance of this file.
+	 *
+	 * @param string $input
+	 * @param string $output
+	 * @param bool $is_dev
+	 *
+	 * @throws Exception
+	 */
+	public function __construct( string $input, string $output, bool $is_dev ) {
+
+		$this->is_dev = $is_dev;
+
+		// TODO  are both necessary?
+		if (!file_exists($input) || !is_readable($input)) {
+			throw new Exception('InputFileNotReadable');
+		}
+
+		// TODO  finish … wrong location?
+	//	if (!file_exists($output) || !is_readable($output)) {
+	//		throw new Exception('OutputFileNotWritable');
+	//	}
+
+
+		$this->input_file = $input;  // discard?
+		$this->output_file = $output;
+
+
+		$this->dom = new DOMDocument();
+
+		$this->dom->load($input);
+		// TODO  trycatch.
+
+		$svg = $this->dom->getElementsByTagName('svg');
+
+		if ($svg->length !== 1) {
+			throw new Exception('NoCleanSvg');
+		}
+
+		$this->root = $svg[0];
+	}
+
+
+
+	// -----
+
+	/**
+	 * @param DOMElement $node
+	 * @return void
+	 */
+	private function removeHiddenChildren( mixed $node ) {
+
+		if (!($node instanceof DOMElement)) {
+			return;
+		}
+
+
+		$deletables = array_filter(
+			iterator_to_array($node->childNodes),
+			fn($node) => $node instanceof DOMElement
+			&&	$node->hasAttribute('display')
+			&&	$node->getAttribute('display') === 'none'
+		);
+
+		foreach ($deletables as $child) {
+			$child->parentNode->removeChild($child);
+		}
+	}
+
+
+	/**
+	 * @param DOMElement $node
+	 * @return void
+	 */
+	private function recurse( mixed $node ) {
+
+		// TODO  node or element?
+
+		if (!($node instanceof DOMElement)) {
+			return;
+		}
+
+		if (!$node->childNodes->length) {
+			return;
+		}
+
+		$this->removeHiddenChildren($node);
+
+		foreach ($node->childNodes as $child) {
+			$this->recurse($child);
+		};
+	}
+
+
+	// -----
+
+
+
+
+	/**
+	 * @throws Exception
+	 *
+	 * @return void
+	 */
+	public function optimize() {
+
+		$this->recurse($this->root);
+
+		// TODO  remove superfluous whitespace? Or already done automatically?
+	}
+
+
+	/**
+	 * It's a dev thing. You wouldn't understand …
+	 *
+	 * @return string
+	 *   Returns HTML content of whole document.
+	 */
+	public function dump() {
+
+		return $this->dom->saveHTML();
+	}
+
+
+	/**
+	 * @throws Exception
+	 *
+	 * @return void
+	 */
+	public function save() {
+
+		$content = $this->dom->saveXML();
+
+		if (FALSE) {
+			// is FALSE?
+			throw new Exception('XmlKaputt');
+		}
+
+		file_put_contents($this->output_file, $content);
+		// TODO  and/or here?
+	}
+
+
+
+
+
+
+
+	// GETTERS
+
+
+	/**
+	 * @return string
+	 */
+	public function getInputFile() {
+		return $this->input_file;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getOutputFile() {
+		return $this->output_file;
+	}
+}
+
