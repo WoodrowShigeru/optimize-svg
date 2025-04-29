16f22ebad450039b55415a9877fa74a48b07b3a8
Woodrow Shigeru <woodrow.shigeru@gmx.net>
2025-04-29 20:56:31 +0200

    created PHP dev environment ::: remove hidden first-level children

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+require_once implode(DIRECTORY_SEPARATOR, [
+	$_SERVER['DOCUMENT_ROOT'],
+	'..',
+	'ivd',
+	'autoload.php',
+]);
+
+
+$input_file = implode(DIRECTORY_SEPARATOR, ['.', 'examples', 'tiles.svg']);
+$output_file = implode(DIRECTORY_SEPARATOR, ['.', 'output.svg']);
+
+ivd([
+	'file-exists'	=> file_exists($input_file),
+	'readable'		=> is_readable($input_file),
+]);
+
+
+// -----
+
+function remove_hidden_stuff_from_nodelist( $node ) {
+
+	$homunculus = new DOMNode();
+
+	if (!($node instanceof DOMNode)) {
+		return;
+		return $homunculus;
+	}
+
+	$deletables = [];
+
+	foreach ($node->childNodes as $child) {
+		$is_hidden = $child->hasAttribute('display') && $child->getAttribute('display') === 'none';
+		// ivd(compact('is_hidden'));
+
+		if ($is_hidden) {
+			$deletables[] = $child;
+		}
+	}
+
+	foreach ($deletables as $child) {
+		$child->parentNode->removeChild($child);
+	}
+
+	return;
+	return $homunculus;
+}
+
+// -----
+
+$dom = new DOMDocument();
+
+$dom->load($input_file);
+
+$root = $dom->getElementsByTagName('svg');
+
+if ($root->length !== 1) {
+	throw new Exception('NoCleanSvg');
+}
+
+ivd($root[0]->childNodes->length, "before");
+
+$samba = remove_hidden_stuff_from_nodelist($root[0]);
+
+$samba = $root[0];
+ivd($samba->childNodes->length, "after");
+
+ivd($dom->saveHTML());
+
+// include $input_file;
+
